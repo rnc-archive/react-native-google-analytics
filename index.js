a98ec2ca50a244750abc18a6e5788ef9f0e3745c
Vanessa Sabino <vanessa.sabino@shopify.com>
2015-08-27 17:37:13 -0400

    implement ecommerce tracking

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ module.exports = {
     PageView: require('./lib/hits/PageView'),
     ScreenView: require('./lib/hits/ScreenView'),
     Social: require('./lib/hits/Social'),
-    Timing: require('./lib/hits/Timing')
+    Timing: require('./lib/hits/Timing'),
+    Transaction: require('./lib/hits/Transaction'),
+    Item: require('./lib/hits/Item')
   },
   Analytics: require('./lib/Analytics'),
   Experiment: require('./lib/Experiment'),
